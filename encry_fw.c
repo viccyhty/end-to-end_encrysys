894eea9fd74781b02b8332852340deac9a04328a
viccyhty <37569422+viccyhty@users.noreply.github.com>
2018-11-18 12:57:32 +0800

    Add files via upload

---
+++ encry_fw.c
@@ -0,0 +1,190 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <linux/types.h>
+#include <linux/netfilter.h>
+#include <libnetfilter_queue/libnetfilter_queue.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+
+char * if_name = "ens33";//默认情况下接口端口名为ens33
+int local_ip = 0;//直接记录32位本地IP地址的网络字节序，只用于比较
+int full_encry = 0;//默认情况会加密所有从本机发出的IP包
+
+static int fd;//使用netfilter_queue会使用到的变量
+static struct nfq_handle *h;//使用netfilter_queue会使用到的变量
+static struct nfq_q_handle *qh;//使用netfilter_queue会使用到的变量
+static struct nfnl_handle *nh;//使用netfilter_queue会使用到的变量
+
+
+int get_local_ip(char *);
+void tcp_encry(char *,int ,int );
+
+static int callback(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,
+struct nfq_data *nfa, void *data)
+//回调函数
+{
+	int id = 0;//IP包在队列中的序号
+	struct nfqnl_msg_packet_hdr *ph;
+	unsigned char *pdata = NULL;
+	int pdata_len, iphdr_size;
+	char srcstr[32], deststr[32];
+	struct iphdr *piphdr;
+
+//返回数据包nf头文件(队列ID)	
+	ph = nfq_get_msg_packet_hdr(nfa);
+	if(ph == NULL)
+		return 1;
+	id = ntohl(ph ->packet_id);
+
+//获取IP层发来的报文
+	pdata_len = nfq_get_payload(nfa, (unsigned char **)&pdata);
+	if (pdata != NULL)
+		piphdr = (struct iphdr *) pdata;//转换为IP头指针，获得IP头部
+	else
+		return 1;
+	iphdr_size = piphdr->ihl << 2;//ip包头部长度
+
+//将IP头部中的源地址和目的地至转化为点分式的字符串格式，供输出显示	
+	inet_ntop(AF_INET, &(piphdr->saddr), srcstr, 32);
+	inet_ntop(AF_INET, &(piphdr->daddr), deststr, 32);
+	printf("get a packet: %s -> %s\n", srcstr, deststr);
+
+	if(piphdr->saddr == local_ip)
+	{
+		printf("The packet is locally generated. Jump to encryption program.\n");
+		if(piphdr->protocol == 6)//tcp报文
+		{
+			tcp_encry(pdata, pdata_len, iphdr_size);
+			return nfq_set_verdict(qh, id, NF_ACCEPT, (u_int32_t)pdata_len, pdata);
+		}	
+		else if(piphdr->protocol == 17)//UDP报文
+		{
+			udp_entry(pdata, pdata_len, iphdr_size);
+			return nfq_set_verdict(qh, id, NF_ACCEPT, (u_int32_t)pdata_len, pdata);
+		}
+	}
+
+	if(piphdr->daddr == local_ip)
+	{
+		printf("The packet is sent to local. Jump to decryption program.\n");
+		if(piphdr->protocol == 6)//tcp报文
+		{
+			tcp_decry(pdata, pdata_len, iphdr_size);
+			return nfq_set_verdict(qh, id, NF_ACCEPT, (u_int32_t)pdata_len, pdata);
+		}	
+		else if(piphdr->protocol == 17)//UDP报文
+		{
+			udp_detry(pdata, pdata_len, iphdr_size);
+			return nfq_set_verdict(qh, id, NF_ACCEPT, (u_int32_t)pdata_len, pdata);
+		}
+	}
+
+	return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);
+
+}
+
+
+int main(int argc, char **argv)
+{ 
+    char buf[1600] __attribute__((aligned));
+    int length;
+	
+	local_ip = get_local_ip(if_name);
+	
+//	opening library handle
+	h = nfq_open();
+	if (!h) {
+		fprintf(stderr, "error during nfq_open()\n");
+		exit(1);
+	}
+
+//	unbinding existing nf_queue handler for AF_INET (if any) 解除绑定
+	if (nfq_unbind_pf(h, AF_INET) < 0) {
+		fprintf(stderr, "already nfq_unbind_pf()\n");
+		exit(1);
+	}
+
+//	binding nfnetlink_queue as nf_queue handler for AF_INET 重新绑定协议簇
+	if (nfq_bind_pf(h, AF_INET) < 0) {
+		fprintf(stderr, "error during nfq_bind_pf()\n");
+		exit(1);
+	}
+
+//	选择处理队列号以及设置回调函数
+	qh = nfq_create_queue(h,0, &callback, NULL);
+	if (!qh) {
+		fprintf(stderr, "error during nfq_create_queue()\n");
+		exit(1);
+	}
+
+//	设置参数模式（返回数据包）
+	if (nfq_set_mode(qh, NFQNL_COPY_PACKET, 0xffff) < 0) {
+		fprintf(stderr, "can't set packet_copy mode\n");
+		exit(1);
+	}
+
+	nh = nfq_nfnlh(h);
+	fd = nfnl_fd(nh);
+   	while(1)
+    	{
+		length=recv(fd,buf,1600,0);//此处完成收包
+		nfq_handle_packet(h, buf,length);//完成发包的真正函数
+	}
+	nfq_destroy_queue(qh);
+	nfq_close(h);
+	exit(0);
+}
+
+
+//得到本地IP地址的32位int型数据,只用于比较
+int get_local_ip(char * ifname)
+{
+    int inet_sock;
+    struct ifreq ifr;
+	int *temp;
+
+//初始化套接字以及设置结构体ifreq
+    inet_sock = socket(AF_INET, SOCK_DGRAM, 0); 
+
+    memset(ifr.ifr_name, 0, sizeof(ifr.ifr_name));
+    memcpy(ifr.ifr_name, ifname, strlen(ifname));
+
+    if(0 != ioctl(inet_sock, SIOCGIFADDR, &ifr)) //申请得到IP地址
+    {   
+        perror("ioctl error");
+        return -1;
+    }
+
+//得到32位的ip地址数据
+    temp = (unsigned int *)&(((struct sockaddr_in*)&(ifr.ifr_addr))->sin_addr);      
+
+    close(inet_sock);
+
+    return *temp;
+}
+
+
+//针对ｔｃｐ协议的加密
+void tcp_encry(char *pdata, int pdata_len, int iphdr_size)
+{
+	struct tcphdr *tcp_hdr;
+	int tcphdr_size;
+	int content_len;
+
+//获得ＴＣＰ头部,
+	tcp_hdr = (struct tcphdr *)(pdata + iphdr_size);
+	tcphdr_size = (tcp_hdr->doff) << 2;
+	content_len = pdata_len - iphdr_size - tcphdr_size;
+	char *a = pdata + iphdr_size + tcphdr_size;
+
+	if(content_len > 0)
+	{
+		printf("%s\n", a);
+	}
+}
